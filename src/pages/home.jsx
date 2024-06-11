58fbce86d7ffde72220d4e54dae6ede944a366ac
Vinicius Paixão <vinisantospb404@gmail.com>
2024-06-11 01:34:00 +0000

    style: posicionamento do card no centro da tela

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,44 @@
-export default function home() {
-  return (
-    <div>home</div>
-  )
+import { ButtonConteiner, CardContainer, IconConteiner, ResultConteiner } from "./Home.styles";
+
+import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
+import { useState } from "react";
+
+export function Home() {
+    const [mostrarResultado, setMostrarResutado] = useState(false)
+    const [feedbackNote, setfeedbackNote] = useState(0)
+
+    function handleFeedbackButtonClick(event) {
+        const feedback = Number(event.target.innerText) 
+
+        setfeedbackNote(feedback)
+      }
+
+    function handleSubmit() {
+        if (feedbackNote === 0) return
+
+        setMostrarResutado(true)
+    }
+    return (
+        mostrarResultado === false ? (
+            <CardContainer>
+                <IconConteiner>
+                    <img src={iconStarImg} alt="ícone de estrela" />
+                </IconConteiner>
+
+                <h1>Como foi o atendimento?</h1>
+                <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+
+                <ButtonConteiner> 
+
+                    <button >1</button>
+                    <button >2</button>
+                    <button >3</button>
+                    <button >4</button>
+                    <button >5</button>
+
+                </ButtonConteiner>
+<button onClick={handleSubmit}>Enviar</button>
+            </CardContainer>
+        ) 
 }
---
+++ src/styles/home.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Overpass", sans-serif;
+    }
+    
+    :root {
+        font-size: 62.5%;
+    }
+
+    body {
+        font-size: 1.6rem;
+        background: ${props => props.theme.homeBackground};
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+}
+`
