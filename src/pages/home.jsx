87ee80564d0b8a10665355b1aa54735ff0c83949
Vinicius Paixão <vinisantospb404@gmail.com>
2024-06-11 01:44:09 +0000

    style: estilalização do h1 e do p do card

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,20 +10,20 @@ export function Home() {
 
     function handleFeedbackButtonClick(event) {
         const feedback = Number(event.target.innerText) 
-
+    
         setfeedbackNote(feedback)
       }
-
+    
     function handleSubmit() {
         if (feedbackNote === 0) return
-
+    
         setMostrarResutado(true)
     }
     return (
         mostrarResultado === false ? (
             <CardContainer>
                 <IconConteiner>
-                    <img src={iconStarImg} alt="ícone de estrela" />
+                    <img src={iconStarImg} alt="ícone de estrela" />  
                 </IconConteiner>
 
                 <h1>Como foi o atendimento?</h1>
@@ -31,14 +31,28 @@ export function Home() {
 
                 <ButtonConteiner> 
 
-                    <button >1</button>
-                    <button >2</button>
-                    <button >3</button>
-                    <button >4</button>
-                    <button >5</button>
+                    <button onClick={handleFeedbackButtonClick}>1</button>
+                    <button onClick={handleFeedbackButtonClick}>2</button>
+                    <button onClick={handleFeedbackButtonClick}>3</button>
+                    <button onClick={handleFeedbackButtonClick}>4</button>
+                    <button onClick={handleFeedbackButtonClick}>5</button>
 
                 </ButtonConteiner>
-<button onClick={handleSubmit}>Enviar</button>
+
+                <button onClick={handleSubmit}>Enviar</button>
             </CardContainer>
-        ) 
+        ) : (
+            <CardContainer>
+                <img src={thankYouImg} alt="imagem de agradecimento" />
+
+                <ResultConteiner>
+                    <p>Você selecionou {feedbackNote} de 5</p>
+                </ResultConteiner>
+
+                <h1>Obrigado!</h1>
+
+                <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato! </p>
+            </CardContainer>
+        )
+    )
 }
