ff8ebc18fc8862d2bf0660c7f7f01bdf63981e4e
Vinicius Paixão <vinisantospb404@gmail.com>
2024-06-11 01:14:28 +0000

    Style: criação dos eestilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "styled-components"
 
 import { Home } from "./pages/Home"
 import { theme } from "./styles/theme"
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle } from "./styles/global.js"
 
 const router = createBrowserRouter([
   {
